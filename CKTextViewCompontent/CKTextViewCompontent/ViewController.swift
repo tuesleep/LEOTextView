2321a08029faabddb7a901e8645143fdf3df754a
chanricle <chanricle@gmail.com>
2016-04-27 22:09:42 +0800

    init project.

---
+++ CKTextViewCompontent/CKTextViewCompontent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CKTextViewCompontent
+//
+//  Created by Chanricle King on 16/4/27.
+//  Copyright © 2016年 chanricle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
