3ce498ea71ec58de7f8b264207a8cb5dcc4c5627
Chanricle King <chanricle@icloud.com>
2016-05-17 11:33:54 +0800

    add reveal pod support.

--- CKTextView.podspec
+++ CKTextView.podspec
@@ -17,7 +17,7 @@ Pod::Spec.new do |s|
 #   * Write the description between the DESC delimiters below.
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
-  s.description      = "Want to build a component that like UITextView and iPhone 'Notes' app."
+  s.description      = "Want to build a UITextView that have List feature, just look like 'Notes' app on the iPhone with iOS 9."
 
   s.homepage         = "https://github.com/chanricle/CKTextView"
   # s.screenshots     = "www.example.com/screenshots_1", "www.example.com/screenshots_2"
--- CKTextView/Classes/ListPrefix/CheckBoxListItem.swift
+++ CKTextView/Classes/ListPrefix/CheckBoxListItem.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CheckBoxListItem: BaseListItem {
     var button: UIButton?
+    var isChecked = false
     
     override func listType() -> ListType {
         return ListType.Bulleted
@@ -33,7 +34,7 @@ class CheckBoxListItem: BaseListItem {
         }
     }
     
-    // MARK: Override
+    // MARK: - Override
     
     override func createNextItemWithY(y: CGFloat, ckTextView: CKTextView) {
         let nextItem = CheckBoxListItem(keyY: y, ckTextView: ckTextView, listInfoStore: self.listInfoStore)
@@ -52,17 +53,44 @@ class CheckBoxListItem: BaseListItem {
         button?.removeFromSuperview()
     }
     
-    // MARK: setups
+    // MARK: - Setups
     
     private func setupCheckBoxButton(keyY: CGFloat, ckTextView: CKTextView)
     {
         let lineHeight = ckTextView.font!.lineHeight
+        let distance = lineHeight - 4
         
-        button = UIButton(frame: CGRect(x: 8, y: keyY, width: lineHeight, height: lineHeight))
-        button!.setBackgroundImage(UIImage(named: "icon-checkbox-normal"), forState: .Normal)
-        button!.setImage(UIImage(named: "icon-checkbox-normal"), forState: .Normal)
+        button = UIButton(frame: CGRect(x: 12, y: keyY + 3, width: distance, height: distance))
+        button!.addTarget(self, action: #selector(self.checkBoxButtonAction(_:)), forControlEvents: .TouchUpInside)
+        
+        changeCheckBoxButtonBg()
         
         // Append label to textView.
         ckTextView.addSubview(button!)
+    }
+    
+    // MARK: - Button Action
+    
+    func checkBoxButtonAction(checkBoxButton: UIButton) {
+        isChecked = !isChecked
+        changeCheckBoxButtonBg()
+    }
+    
+    // MARK: -
+    
+    func changeCheckBoxButtonBg() {
+        let name = isChecked ? "icon-checkbox-checked.png" : "icon-checkbox-normal.png"
+        
+        let bundleURL = NSBundle.mainBundle().pathForResource("CKTextView", ofType: "bundle")
+        print("bundleURL: \(bundleURL)")
+        
+        /*
+        if let bundleURL = NSBundle.mainBundle().pathForResource("CKTextView", ofType: "bundle") {
+            let bundle = NSBundle(path: bundleURL)
+            let backgroundImage = UIImage(named: name, inBundle: bundle, compatibleWithTraitCollection: nil)
+            print("checkbox button bg image: \(backgroundImage)")
+            button!.setBackgroundImage(backgroundImage, forState: .Normal)
+        }
+         */
     }
 }
