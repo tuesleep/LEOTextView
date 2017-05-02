96a15115b06e4629ce0aa408f438a65c108b9369
Leonardo Hammer <leonardo_hammer@zoho.com>
2017-05-02 17:19:36 +0800

    Updated git ignore file, added Pod directory.

---
+++ Example/Pods/Target Support Files/LEOTextView/LEOTextView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LEOTextView : NSObject
+@end
+@implementation PodsDummy_LEOTextView
+@end
---
+++ Example/Pods/Target Support Files/Pods-LEOTextView_Example/Pods-LEOTextView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LEOTextView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LEOTextView_Example
+@end
